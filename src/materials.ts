506934123c1e01fc915476826721b921836d26ad
Adam Leis <aleis@iti4dmv.com>
2025-05-27 22:04:00 -0400

    complete materials

--- src/materials.ts
+++ src/materials.ts
@@ -1,4 +1,4 @@
-import { MeshBasicMaterial, Mesh, SphereGeometry, PlaneGeometry, TorusGeometry, AmbientLight, PerspectiveCamera, Scene, WebGLRenderer, Clock, TextureLoader, SRGBColorSpace, Color, DoubleSide, MeshNormalMaterial, MeshMatcapMaterial, LoadingManager, MeshLambertMaterial, PointLight, MeshPhongMaterial, MeshToonMaterial, MeshStandardMaterial, EquirectangularReflectionMapping } from 'three'
+import { MeshBasicMaterial, Mesh, SphereGeometry, PlaneGeometry, TorusGeometry, AmbientLight, PerspectiveCamera, Scene, WebGLRenderer, Clock, TextureLoader, SRGBColorSpace, Color, DoubleSide, MeshNormalMaterial, MeshMatcapMaterial, LoadingManager, MeshLambertMaterial, PointLight, MeshPhongMaterial, MeshToonMaterial, MeshStandardMaterial, EquirectangularReflectionMapping, MeshPhysicalMaterial } from 'three'
 import './style.css';
 import GUI from 'lil-gui';
 import { OrbitControls, RGBELoader } from 'three/examples/jsm/Addons.js';
@@ -34,7 +34,7 @@ scene.add(light, pointLight);
 
 // Environment Map
 const rgbeLoader = new RGBELoader();
-rgbeLoader.load('/forest-env-map.hdr', envMap => {
+rgbeLoader.load('/sky-env-map.hdr', envMap => {
     envMap.mapping = EquirectangularReflectionMapping;
     scene.background = envMap;
     scene.environment = envMap;
@@ -78,29 +78,62 @@ matcapTexture.colorSpace = SRGBColorSpace;
 // material.specular = new Color('#18f');
 // const material = new MeshToonMaterial()
 // material.gradientMap = gradientTe
-const material = new MeshStandardMaterial();
-material.metalness = 0.7;
-material.roughness = 0.2;
+// const material = new MeshStandardMaterial();
+const material = new MeshPhysicalMaterial();
+material.side = DoubleSide;
+material.metalness = 1;
+material.roughness = 1;
+// material.map = colorTexture;
+// material.aoMap = ambientTexture;
+// material.aoMapIntensity = 1;
+// material.displacementMap = heightTexture;
+// material.displacementScale = 0.02;
+// material.metalnessMap = metalnessTexture;
+// material.roughnessMap = roughnessTexture;
+// material.normalMap = normalTexture;
+// material.transparent = true;
+// material.alphaMap = alphaTexture;
 
-material.map = colorTexture;
-material.aoMap = ambientTexture;
+// material.clearcoat = 1;
+// material.clearcoatRoughness = 0;
+
+// material.sheen = 1;
+// material.sheenRoughness = 0.25;
+// material.sheenColor.set(1, 1, 1);
+
+// material.iridescence = 1;
+// material.iridescenceIOR = 1;
+// material.iridescenceThicknessRange = [100, 800];
+
+material.transmission = 1;
+material.ior = 1.5;
+material.thickness = 0.5;
+
+gui.add(material, 'transmission').min(0).max(1).step(0.0001);
+gui.add(material, 'ior').min(1).max(10).step(0.0001);
+gui.add(material, 'thickness').min(0).max(1).step(0.0001);
+
+// gui.add(material, 'iridescence').min(0).max(1).step(0.0001);
+// gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.0001);
+// gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1);
+// gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1);
 
 gui.add(material, 'metalness').min(0).max(1).step(0.001);
 gui.add(material, 'roughness').min(0).max(1).step(0.001);
 
 const sphere = new Mesh(
-    new SphereGeometry(0.5, 16, 16),
+    new SphereGeometry(0.5, 64, 64),
     material
 )
 sphere.position.x = - 1.5
 
 const plane = new Mesh(
-    new PlaneGeometry(1, 1),
+    new PlaneGeometry(1, 1, 100, 100),
     material
 )
 
 const torus = new Mesh(
-    new TorusGeometry(0.3, 0.2, 16, 32),
+    new TorusGeometry(0.3, 0.2, 64, 128),
     material
 )
 torus.position.x = 1.5;
@@ -114,12 +147,12 @@ function animate() {
     const elapsedTime = clock.getElapsedTime();
 
     // sphere.rotation.y = 0.1 * elapsedTime;
-    // plane.rotation.y = 0.1 * elapsedTime;
-    // torus.rotation.y = 0.1 * elapsedTime;
+    plane.rotation.y = 0.1 * elapsedTime;
+    torus.rotation.y = 0.1 * elapsedTime;
 
     // sphere.rotation.x = 0.15 * elapsedTime;
-    // plane.rotation.x = 0.15 * elapsedTime;
-    // torus.rotation.x = 0.15 * elapsedTime;
+    plane.rotation.x = 0.15 * elapsedTime;
+    torus.rotation.x = 0.15 * elapsedTime;
 
     controls.update();
 
